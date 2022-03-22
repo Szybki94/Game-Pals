35d8318250074bbbefd5f11cc41af372355a8502
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2022-03-22 21:23:24 +0100

    Added and configured static files

--- Game_Pals/main_app/views.py
+++ Game_Pals/main_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
+
 # Create your views here.
 class TEST(View):
     def get(self, request):
-        return HttpResponse("<h1>TEST</h1> TEST")
+        return render(request, "base.html", {})
